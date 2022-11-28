44f322c149a6b88ac6e82729b76bf57406134700
Tate <thandley1234@gmail.com>
2022-11-27 17:14:23 -0700

    commit tree b957a1590a3058d56cde1336032be2fe4df17581

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+// const destructivelyAppendCat = ["Ralph"];
+function destructivelyAppendCat(name){
+    (cats.push(name))
+}
+function destructivelyPrependCat(name){
+    (cats.unshift(name))
+}
+function destructivelyRemoveLastCat(name){
+    (cats.pop(name))
+}
+function destructivelyRemoveFirstCat(name){
+    (cats.shift(name))
+}
+function appendCat(name){
+    // (cats.slice(...name, cats))
+   return [...cats, name] 
+//    const catsCopy = cats.slice()
+    // catsCopy.push(name) <== option 2 to get similar result
+    // return catsCopy
+}
+function prependCat(name){
+    return [name, ...cats]
+}
+function removeLastCat(name){
+   return cats.slice(0, -1)
+}
+function removeFirstCat(name){
+    return cats.slice(1)
+}
